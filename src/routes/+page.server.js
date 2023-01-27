766052c7dde689d1e6bc121b6ee6be91d6154df8
GleemingKnight <7jashton@gmail.com>
2023-01-27 01:52:47 -0500

    Change URLs from local to public

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,12 +1,12 @@
 export async function load({params}) {
-    let response = await fetch("http://localhost:5173/data/teams/all_teams.json");
+    let response = await fetch("https://rocket-league-teams.vercel.app/data/teams/all_teams.json");
     let responseJson = await response.json()
 
     let teams = [];
 
     for (let team in responseJson.teams) {
         let teamName = responseJson.teams[team];
-        let rawData = await fetch("http://localhost:5173/data/teams/" + teamName + ".json")
+        let rawData = await fetch("https://rocket-league-teams.vercel.app/data/teams/" + teamName + ".json")
         let json = await rawData.json()
         teams.push(json)
     }
--- src/routes/teams/[slug]/+page.server.js
+++ src/routes/teams/[slug]/+page.server.js
@@ -1,7 +1,7 @@
 import {error} from "@sveltejs/kit";
 
 export async function load({params}) {
-    let response = await fetch("http://localhost:5173/data/teams/" + params.slug.toLowerCase() + ".json");
+    let response = await fetch("https://rocket-league-teams.vercel.app/data/teams/" + params.slug.toLowerCase() + ".json");
 
     if (!response.ok) {
         throw error(404, "Not found")
